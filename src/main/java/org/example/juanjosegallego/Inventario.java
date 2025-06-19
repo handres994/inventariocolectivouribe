a1566643d3c963d5a780d66638cec3ed159944f1
handres994 <handres994@gmail.com>
2025-06-19 10:56:10 -0500

    feat se programa ciclo con exception

--- src/main/java/org/example/juanjosegallego/Inventario.java
+++ src/main/java/org/example/juanjosegallego/Inventario.java
@@ -24,8 +24,21 @@ public class Inventario {
         System.out.println("4️⃣  Eliminar una prenda del sistema 🗑️");
         System.out.println("5️⃣  Presiona 5 para SALIR ❌");
 
-        while(menuOption!=5){
-            System.out.println("oe");
+        System.out.println("Digita una opcion: ");
+        menuOption=keyEntry.nextInt();
+
+        try {
+            if (){
+
+            } else if () {
+
+            } else if () {
+
+            } else if () {
+
+            }
+        }catch (Exception error){
+
         }
 
         //PROCESO 2
